e3b411f88dd3b72cbaf7dedd719c98218035abb8
Andy Pan <andy0130tw@yahoo.com.tw>
2025-07-03 00:38:14 +0800

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { parseFormatString } from './fmtstr'
+export { parseFormatString, getEmittedTokens } from './fmtstr'
 export { Parser } from './parser'
 export { decodeSlice } from './unpacker'
 export { stringToCell } from './utils'
